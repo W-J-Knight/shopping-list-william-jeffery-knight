4ebd8b8a45727756151617e428bdb62a22ced10b
William Knight <William.Jeffery.Knight@protonmail.com>
2024-03-25 09:32:22 -0400

    Video 98 Clear UI State. Remove Filter and Clear All button if on list items. And be able to add items back to the DOM.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
-const clearBtn = document.getElementById("clear")
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 function addItem(e) {
   e.preventDefault();
@@ -21,7 +22,10 @@ function addItem(e) {
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
+  //   Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
 
   itemInput.value = "";
 }
@@ -39,21 +43,36 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
 
-function removeItem(e){
-    if(e.target.parentElement.classList.contains('remove-item')){
-        e.target.parentElement.parentElement.remove()
-    };
-
-}
-
-function clearItems(){
-    while(itemList.firstChild){
-        itemList.removeChild(itemList.firstChild)
+      checkUI();
     }
+  }
 }
 
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = document.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
-itemList.addEventListener('click', removeItem);
-clearBtn.addEventListener('click', clearItems)
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+
+checkUI();
